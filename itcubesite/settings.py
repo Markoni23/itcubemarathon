f1a6077192e1e9a153f173106e460f92f2fdeb66
Mark Zhigulin <forsteam.markoni@mail.ru>
2020-04-12 12:41:24 +0300

    1

--- itcubesite/settings.py
+++ itcubesite/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'itcubesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itcube',
+        'USER': 'itcube',
+        'PASSWORD': 'kmnuZ277',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 20,
     }
 }
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
