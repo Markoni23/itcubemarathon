01f85a164d365aca47ecf994bc29272d3c19e7ac
Марк Жигулин <markzigulin@MacBook-Air-Semen.local>
2020-04-19 19:17:59 +0300

    buttons

--- itcubesite/settings.py
+++ itcubesite/settings.py
@@ -142,15 +142,8 @@ LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
 
-STATIC_URL = '/static/'
-
 #location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-# location where you will store your static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'itcubesite/static')
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
